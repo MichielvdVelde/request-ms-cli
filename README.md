eb28ae54cba510505bf2b27347dcabb08180f4ac
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-12-01 09:21:56 +0100

    V0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # request-ms-cli
-A simple CLI app that allows you to measure the response time with simple HEAD request
+
+This is the CLI app for the [request-ms](http://github.com/MichielvdVelde/request-ms) module. Just like the module it allows you to get the response time for a remote endpoint. It's a `ping` for HTTP(S) host.
+
+For more info, read the [request-ms](http://github.com/MichielvdVelde/request-ms) readme.
+
+# Install
+
+	npm install -g request-ms-cli
+
+# Usage
+
+    $> request-ms example.com
+
+What you will get is something similar to this:
+
+    HEAD example.com - 200 - 43.45643ms
+
+See the help (`request-ms -h`) for all available options.
+
+# Version history
+
+* 0.0.1
+  * Moved CLI app to separate package from [request-ms](http://github.com/MichielvdVelde/request-ms)
+
+# License
+
+Copyright 2015 Michiel van der Velde.
+
+This software is licensed under the [MIT License](LICENSE)
---
+++ bin/request-ms.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+var pkg = require('../package');
+var request = require('request-ms');
+var program = require('commander');
+
+var url = require('url');
+var format = require('util').format;
+
+/**
+ * Method to display the status message
+*/
+var displaySuccessMessage = function(message) {
+	console.log();
+	console.log('  ' + message);
+};
+
+/**
+ * Method to display an error message
+*/
+var displayErrorMessage = function(message) {
+	console.error();
+	console.error(message);
+};
+
+/**
+ * Program and options definition
+*/
+program.version(pkg.version)
+	.arguments('<host>')
+	.option('--https', 'Use HTTPS (default HTTP)')
+	.option('--timeout [timeout]', 'Timeout in ms (default 2500)', 2500)
+	.option('--method [method]', 'Method to use (default HEAD)', 'HEAD')
+	.parse(process.argv);
+
+program.hostname = program.args[0] || null;
+if(!program.hostname)
+	return displayErrorMessage('Error: No host specified');
+
+if(program.hostname.substr(0, 7).toLowerCase() != 'http://')
+	program.hostname = 'http://' + program.hostname;
+
+var urlOptions = url.parse(program.hostname);
+urlOptions.method = program.method;
+
+request(urlOptions, { 'timeout': program.timeout || 2500 }, function(err, response) {
+	if(err)
+		return displayErrorMessage('Got an error:', err.message);
+	return displaySuccessMessage(format('%s %s - %d - %dms', urlOptions.method, urlOptions.host, response.statusCode, response.elapsed));
+});
